e9d00d7d463fbe714b7a0945d28b64ba2158affa
JohanAntonyAS <64118063+JohanAntonyAS@users.noreply.github.com>
2022-06-27 00:38:27 -0500

    agregando cambios y compatibilidad

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 let $video = document.querySelector('#video');
 let $play = document.querySelector('#play');
 let $backward = document.querySelector('#backward');
@@ -9,73 +11,81 @@ let tooltip = document.getElementById('tooltip');
 let tValue = document.getElementById('tValue');
 let $widthBar = document.documentElement.style;
 let xyz;
-
 $play.addEventListener("click", handlePlay);
 
 function handlePlay() {
-    $video.play()
-    $play.hidden = true
-    $pause.hidden = false
+    $video.play();
+    $play.hidden = true;
+    $pause.hidden = false;
+
     if (porcent >= "100") {
-        tooltip.style.left = "3.5%"
+        tooltip.style.left = "3.5%";
     }
 }
+
 $pause.addEventListener("click", handlePause);
+
 function handlePause() {
-    $video.pause()
-    $play.hidden = false
-    $pause.hidden = true
+    $video.pause();
+    $play.hidden = false;
+    $pause.hidden = true;
+
     if (porcent >= "3.50") {
         tooltip.style.left = porcent + "%";
     }
+
     if (porcent >= "97.5") {
         tooltip.style.left = "97.5%";
     }
 }
-$backward.addEventListener('click', handleBackward)
+
+$backward.addEventListener('click', handleBackward);
+
 function handleBackward() {
-    $video.currentTime = $video.currentTime - 10
-
-    tValue.innerHTML = secondsToString($video.currentTime);
-    console.log('retroceder', $video.currentTime)
+    $video.currentTime = $video.currentTime - 10;
+    tValue.innerHTML = secondsToString($video.currentTime); // console.log('retroceder', $video.currentTime)
 
     if (porcent >= "3.50") {
         tooltip.style.left = porcent + "%";
     }
+
     if (porcent >= "97.5") {
         tooltip.style.left = "97.5%";
     }
 }
-$forward.addEventListener('click', handleForward)
+
+$forward.addEventListener('click', handleForward);
+
 function handleForward() {
-    $video.currentTime = $video.currentTime + 10
-    tValue.innerHTML = secondsToString($video.currentTime);
-    console.log('adelante', $video.currentTime)
+    $video.currentTime = $video.currentTime + 10;
+    tValue.innerHTML = secondsToString($video.currentTime); // console.log('adelante', $video.currentTime)
+
     if (porcent >= "3.50") {
         tooltip.style.left = porcent + "%";
     }
+
     if (porcent >= "97.5") {
         tooltip.style.left = "97.5%";
     }
 }
 
-$video.addEventListener('loadedmetadata', handleLoaded)
-$video.addEventListener('timeupdate', handleTimeUpdate)
-$progress.addEventListener('input', handleInput)
+$video.addEventListener('loadedmetadata', handleLoaded);
+$video.addEventListener('timeupdate', handleTimeUpdate);
+$progress.addEventListener('input', handleInput);
 
 function handleLoaded() {
-    $progress.max = $video.duration
+    $progress.max = $video.duration;
 }
+
 function handleTimeUpdate() {
     $progress.value = $video.currentTime;
-
     tValue.innerHTML = secondsToString($video.currentTime);
-
     porcent = $video.currentTime * 100 / $video.duration;
 
     if (porcent >= "3.50") {
         tooltip.style.left = porcent + "%";
     }
+
     if (porcent >= "97.5") {
         tooltip.style.left = "97.5%";
     }
@@ -87,30 +97,27 @@ tValue.innerHTML = $video.currentTime;
 
 function handleInput() {
     $video.currentTime = $progress.value;
-
     tValue.innerHTML = secondsToString($video.currentTime);
-
     porcent = $video.currentTime * 100 / $video.duration;
 
     if (porcent >= "3.50") {
         tooltip.style.left = porcent + "%";
     }
+
     if (porcent >= "97.5") {
         tooltip.style.left = "97.5%";
     }
 
-    $widthBar.setProperty("--widthBar", `${porcent}%`);
-
-    console.log($video.currentTime + "handleInput")
+    $widthBar.setProperty("--widthBar", `${porcent}%`); // console.log($video.currentTime + "handleInput")
 }
 
 function secondsToString(xyz) {
     var hour = Math.floor(xyz / 3600);
-    hour = (Math.round(hour) <= 9) ? '0' + Math.round(hour) : Math.round(hour);
-    var minute = Math.floor((xyz / 60) % 60);
-    minute = (Math.round(minute) <= 9) ? '0' + Math.round(minute) : Math.round(minute);
+    hour = Math.round(hour) <= 9 ? '0' + Math.round(hour) : Math.round(hour);
+    var minute = Math.floor(xyz / 60 % 60);
+    minute = Math.round(minute) <= 9 ? '0' + Math.round(minute) : Math.round(minute);
     var second = xyz % 60;
-    second = (Math.round(second) <= 9) ? '0' + Math.round(second) : Math.round(second);
-    console.log(second)
+    second = Math.round(second) <= 9 ? '0' + Math.round(second) : Math.round(second); // console.log(second)
+
     return hour + ':' + minute + ':' + second;
 }
